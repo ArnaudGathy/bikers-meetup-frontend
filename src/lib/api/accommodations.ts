d4284940206730f3082d8864d2a9751e06e8ed10
Arnaud Gathy <arnaud.gathy@gmail.com>
2024-06-19 10:06:45 +0200

    Removed wait

--- src/lib/api/accommodations.ts
+++ src/lib/api/accommodations.ts
@@ -3,6 +3,5 @@ import "server-only";
 import prisma from "@/lib/prisma";
 
 export const getAllAccommodations = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
   return prisma.accommodation.findMany();
 };
