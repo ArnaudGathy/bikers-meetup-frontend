ef69c95b86190920d68dcc3bf1dd8f3de91fe4e0
Arnaud Gathy <arnaud.gathy@gmail.com>
2024-07-02 09:46:35 +0200

    Remove cache from get all registrations

--- src/lib/api/registrations.ts
+++ src/lib/api/registrations.ts
@@ -2,7 +2,9 @@
 import "server-only";
 
 import prisma from "@/lib/prisma";
+import { unstable_noStore as noStore } from "next/cache";
 
 export const getAllRegistrations = async () => {
+  noStore();
   return prisma.registration.findMany();
 };
